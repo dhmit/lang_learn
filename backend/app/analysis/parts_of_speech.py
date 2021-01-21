93373b99aa735f66d6a3f5a65df25942703e16f6
sharabhuiyan <sbhuiyan@mit.edu>
2021-01-21 17:51:08 -0500

    Update parts_of_speech.py

--- backend/app/analysis/parts_of_speech.py
+++ backend/app/analysis/parts_of_speech.py
@@ -1,5 +1,12 @@
 import nltk
 
+# find better way to download wordnet
+try:
+    nltk.data.find('wordnet')
+except LookupError:
+    nltk.download('wordnet', quiet=True)
+from nltk.corpus import wordnet as wn
+
 
 def get_parts_of_speech_tags(text):
     tokens = nltk.word_tokenize(text)
@@ -19,3 +26,20 @@ def get_part_of_speech_words(text, part):
 
     tokens = get_parts_of_speech_tags(text)
     return [token[0] for token in tokens if token[1] in tags[part]]
+
+
+def get_word_definition(word_list):
+    word_def = {}
+    for word in word_list:
+        syns = wn.synsets(word)
+        word_def[word] = syns[0].definition()
+    return word_def
+
+
+def get_word_examples(word_list):
+    word_examples = {}
+    for word in word_list:
+        syns = wn.synsets(word)
+        word_examples[word] = syns[0].examples()
+    return word_examples
+
