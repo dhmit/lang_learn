a2e88c038cd8e55d4d016a5e26f94aaba144ea0d
Jason Lin <jasonlin43212@gmail.com>
2021-01-21 14:29:00 -0500

    Removed not MVP stuff

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -17,30 +17,3 @@ class TextSerializer(serializers.ModelSerializer):
     class Meta:
         model = Text
         fields = ['title', 'text']
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    """
-    Serializes a Student object
-    """
-    class Meta:
-        model = Text
-        fields = ['name', 'username', 'password']
-
-
-class InstructorSerializer(serializers.ModelSerializer):
-    """
-    Serializes an Instructor object
-    """
-    class Meta:
-        model = Text
-        fields = ['name', 'username', 'password']
-
-
-class LeaderboardSerializer(serializers.ModelSerializer):
-    """
-    Serializes a Leaderboard object
-    """
-    class Meta:
-        model = Text
-        fields = ['student', 'score', 'category']
