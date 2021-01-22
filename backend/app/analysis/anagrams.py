30615480d57aacdef0a4ca9767c12cddb3b85e89
Jason Lin <jasonlin43212@gmail.com>
2021-01-22 13:09:29 -0500

    Started working on anagram backend

---
+++ backend/app/analysis/anagrams.py
@@ -0,0 +1,30 @@
+def get_word_set():
+    word_file = './nltk_data/words/en'
+    # word_file = './nltk_data/words/en-basic'
+    with open(word_file) as file:
+        word_set = set(line.strip() for line in file.readlines())
+    return word_set
+
+
+def get_letter_freq(letters):
+    freq = {}
+    for letter in letters:
+        cur_freq = freq.setdefault(letter, 0)
+        freq[letter] = cur_freq + 1
+    return freq
+
+
+def is_anagram(test_freq, word_freq):
+    for letter in test_freq:
+        if letter not in word_freq or test_freq[letter] > word_freq[letter]:
+            return False
+    return True
+
+
+def get_anagrams(anagram_freq):
+    anagrams = set()
+    word_set = get_word_set()
+    for word in word_set:
+        if is_anagram(get_letter_freq(word), anagram_freq):
+            anagrams.add(word)
+    return anagrams
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,8 @@
 """
 These view functions and classes implement API endpoints
 """
+import random
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,6 +11,15 @@ from .models import (
 )
 from .serializers import (
     TextSerializer
+)
+from .analysis.parts_of_speech import (
+    get_part_of_speech_words,
+    get_word_examples,
+    get_word_definition,
+)
+from .analysis.anagrams import (
+    get_anagrams,
+    get_letter_freq,
 )
 
 
@@ -39,4 +50,27 @@ def get_anagram(request, text_id, part_of_speech):
     API endpoint for getting the necessary information for the anagram exercise given
     the id of the text and the part of speech. The anagrams will be random.
     """
+    text_obj = Text.objects.get(id=text_id)
+    words = [word for word in get_part_of_speech_words(text_obj.text, part_of_speech)
+             if "'" not in word]
+    random.shuffle(words)
+    # TODO: Determine how many words from text we should use and which to use
+    words = words[:int(len(words) / 2)]
+
+    definitions = get_word_definition(words)
+    examples = get_word_examples(words)
+
+    # Gets the minimum possible numbers of each letter required to generate all the text
+    anagram_freq = {}
+    for word in words:
+        cur_freq = get_letter_freq(word)
+        for letter in cur_freq:
+            if letter not in anagram_freq:
+                anagram_freq[letter] = cur_freq[letter]
+            elif anagram_freq[letter] < cur_freq[letter]:
+                anagram_freq[letter] = cur_freq[letter]
+    extra_words = get_anagrams(anagram_freq)
+    extra_words -= set(words)  # Remove words from text from extra words
+
+    res = {'extra_words': extra_words}
     return Response("hi")
