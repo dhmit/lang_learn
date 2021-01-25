54e02adba3c0b23fa58d8560843e2bff9b076a6d
Jason Lin <jasonlin43212@gmail.com>
2021-01-25 12:56:23 -0500

    Added docstring for anagram functions

--- backend/app/analysis/anagrams.py
+++ backend/app/analysis/anagrams.py
@@ -3,6 +3,9 @@ from django.conf import settings
 
 
 def get_word_set():
+    """
+    :return: A set of english words
+    """
     word_file = os.path.join(settings.NLTK_WORD_DIR, 'en')
     with open(word_file) as file:
         word_set = set(line.strip() for line in file.readlines())
@@ -10,6 +13,11 @@ def get_word_set():
 
 
 def get_letter_freq(letters):
+    """
+    Given a word, find the frequency of letters in this word
+    :param letters: the word to find the frequency of
+    :return: a dictionary that maps letters to their frequency in the word
+    """
     freq = {}
     for letter in letters:
         letter = letter.lower()
@@ -19,6 +27,11 @@ def get_letter_freq(letters):
 
 
 def is_anagram(test_freq, word_freq):
+    """
+    :param test_freq: The frequency dictionary of the word you are testing
+    :param word_freq: The frequency dictionary that you want the anagram of
+    :return: true if the test word is an anagram
+    """
     for letter in test_freq:
         if letter not in word_freq or test_freq[letter] > word_freq[letter]:
             return False
@@ -26,6 +39,10 @@ def is_anagram(test_freq, word_freq):
 
 
 def get_anagrams(anagram_freq):
+    """
+    :param anagram_freq: the frequency dictionary of the scrambled letters
+    :return: a set of all the anagrams
+    """
     anagrams = set()
     word_set = get_word_set()
     for word in word_set:
