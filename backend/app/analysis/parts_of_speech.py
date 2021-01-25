ab61fd3076e6b8c888e195f870980c8f57f50d3e
Jason Lin <jasonlin43212@gmail.com>
2021-01-25 13:48:59 -0500

    Error handling for bad input

--- backend/app/analysis/parts_of_speech.py
+++ backend/app/analysis/parts_of_speech.py
@@ -160,7 +160,7 @@ def get_part_of_speech_words(text, part):
     part = part.lower()
 
     if part not in tags:
-        return "Part of speech not valid"
+        return []
 
     tokens = get_parts_of_speech_tags(text)
     return [token[0] for token in tokens if token[1] in tags[part]]
