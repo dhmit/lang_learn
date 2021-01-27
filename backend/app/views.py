0134d59b846b47802eea3ade78f5f2f8cba7d183
JusticeV452 <jmvidal@mit.edu>
2021-01-26 22:30:30 -0500

    Updated added get_quiz_data request
    - Added funtion to create sentences (struggles with sentences with punctuations mid-sentence)
    - Added function to create random quiz answer choices
    - Added Text model and serializer
    - Added api request
    - Added __init__.py as verb_conjugation.py and verb.txt from nodebox_linguistics_extended

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,10 +1,23 @@
 """
 These view functions and classes implement API endpoints
 """
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
 
-# from .models import ()
-# from .serializers import ()
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# No API views yet -- write me!
+from .models import (
+    Text
+)
+from .quiz_creation.conjugation_quiz import get_quiz_sentences
+
+
+@api_view(['GET'])
+def get_quiz_data(request, text_id):
+    """
+    API endpoint for getting the necessary information for the verb conjugation quiz given
+    the id of the text. The first verb in each sentence of the text will be fill-in. The options
+    will be randomly selected and arranged.
+    """
+    text_obj = Text.objects.get(id=text_id)
+    res = get_quiz_sentences(text_obj.text)
+    return Response(res)
