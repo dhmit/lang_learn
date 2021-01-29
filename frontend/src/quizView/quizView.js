c2bd550cd59387cfdf954559a44151d60b64e774
L-Jewel <ljmito42@gmail.com>
2021-01-29 15:42:49 -0600

    Added the dummy exit button for the quiz page

--- frontend/src/quizView/quizView.js
+++ frontend/src/quizView/quizView.js
@@ -68,9 +68,13 @@ export class QuizView extends React.Component {
                 {/* <Navbar /> */}
                 <div className="page">
                     <div className="row" id="top">
+                        <div className="exit-button">
+                            <p>&lt;</p>
+                            {/* Eventually, this button will let you leave the quiz */}
+                        </div>
                         <div className="col">
-                            <h1>Verb Conjugation Quiz</h1>
-                            <p><i>by Takako Aikawa</i></p>
+                            <h1 className="quiz-title">Verb Conjugation Quiz</h1>
+                            <p className="quiz-author"><i>by Takako Aikawa</i></p>
                         </div>
                         <div className="col text-right">
                             <Button id="submit" onClick={() => this.nextQuestion()}>
