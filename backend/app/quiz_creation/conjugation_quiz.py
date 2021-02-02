33d80e1d90e461697a4cd28c13efe6edd68e5ea6
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2021-02-02 13:27:38 -0500

    Updated Tests
    Added get_quiz_sentences test
    Updated get_sentence_options test
    Fixed potential index error for remove_contractions

--- backend/app/quiz_creation/conjugation_quiz.py
+++ backend/app/quiz_creation/conjugation_quiz.py
@@ -15,6 +15,7 @@ def get_sentence_options(word):
     :return: A list of 4 words to serve as the answer choices
     """
     options = [word]
+    word_is_capitalized = word[0].isupper()
     # Check if tense is found in the tenses dictionary
     tenses = verb_tenses.get(verb_infinitive(word), '')
     if tenses:
@@ -40,6 +41,12 @@ def get_sentence_options(word):
         # If the verb is not in the verb_tenses dictionary, get three random verb infinitives
         # (keys) from the dictionary
         options += random.sample(list(verb_tenses), 3)
+
+    # Capitalize other options if the original word was capitalized
+    if word_is_capitalized:
+        for i in range(4):
+            options[i] = options[i].capitalize()
+
     random.shuffle(options)
     return options
 
@@ -110,6 +117,7 @@ def get_quiz_sentences(text):
                     )
                     current_sentence['options'] = get_sentence_options(word)
                     current_sentence['answer'] = word
+
             # Add the sentence if it has an answer.
             if current_sentence['answer']:
 
