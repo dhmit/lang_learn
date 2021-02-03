6a8cf5f5df22feac8481b4cfe0cbfd983a9fdd8a
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2021-02-03 05:21:34 -0500

    Added react-tooltip and readded AnagramView to app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { IndexView } from './index/index';
 import { QuizView } from './quizView/quizView';
+import { AnagramView } from './anagramView/anagramView';
 import { AllQuizView } from './quizView/allQuizView';
 
 // Import all styles
@@ -21,5 +22,6 @@ window.app_modules = {
     // Add all frontend views here
     IndexView,
     QuizView,
+    AnagramView,
     AllQuizView,
 };
