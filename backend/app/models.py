3acb7d14e0fd0a8c1106334ec7054f3ae593e944
taokinbo <70776613+taokinbo@users.noreply.github.com>
2021-02-04 15:41:27 -0500

    added new JSON Fields

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,27 @@ Models for the lang_learn app.
 from django.db import models
 
 
+def default_module():
+    mods = {
+        'anagrams': {
+            'noun': False,
+            'verb': False,
+            'adjective': False,
+            'adverb': False,
+        },
+        'flashcards': {
+            'noun': False,
+            'verb': False,
+            'adjective': False,
+            'adverb': False,
+        },
+        'quiz': {
+            'conjugations': False,
+        }
+    }
+    return mods
+
+
 class Text(models.Model):
     """
     This model will hold the piece of text that will be used to generate exercises
@@ -11,3 +32,4 @@ class Text(models.Model):
     """
     text = models.TextField(max_length=5000, null=True)
     title = models.CharField(max_length=252, null=True)
+    modules = models.JSONField(null=True, blank=True, default=default_module())
