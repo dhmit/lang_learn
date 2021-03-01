e3e94f0c6ca6e5754df0ed4df8bced6040a87230
taokinbo <70776613+taokinbo@users.noreply.github.com>
2021-03-01 13:19:48 -0500

    A semi-working crossword

--- backend/app/analysis/crosswords.py
+++ backend/app/analysis/crosswords.py
@@ -3,6 +3,14 @@ Helper functions for generating and checking anagrams
 """
 import os
 import random
+# Standard imports
+import argparse
+
+# Custom imports
+from app.analysis import file_ops
+
+from app.analysis.grid_generator import GridGenerator
+
 from django.conf import settings
 
 
@@ -54,34 +62,119 @@ def get_anagrams(anagram_freq):
             anagrams.add(word.lower())
     return anagrams
 
+
 def rand_words(all_words, max):
     words = set()
-    for i in range(max):
-        indx = random.randint(0, len(all_words))
-        while (words[indx] not in words):
-            words.push(words[indx])
+    for i in range(max + 1):
+        indx = random.randint(0, len(all_words)-1)
+        while all_words[indx] not in words:
+            words.add(all_words[indx])
     return list(words)
 
-def place_word_in_map(word, map, x, y, direction, current_words):
-    "direction: 0 for across, 1 for down"
 
-def make_crossword(map, words, words_left):
+# def place_word_in_map(word, map, x, y, direction, current_words):
+#     "direction: 0 for across, 1 for down"
 
 
-def get_crosswords(all_words):
+def parse_cmdline_args():
+    """ Uses argparse to get commands line args.
+    """
+    parser = argparse.ArgumentParser(description='Generate a crossword puzzle.')
+    parser.add_argument('-f', type=str,
+                        default="words.txt",
+                        dest="word_file",
+                        help="A file containing words, one word per line.")
+    parser.add_argument('-d', type=int,
+                        nargs="+",
+                        default=[20, 20],
+                        dest="dim",
+                        help="Dimensions of the grid to build.")
+    parser.add_argument('-n', type=int,
+                        default=10,
+                        dest="n_loops",
+                        help="NUmber of execution loops to run.")
+    parser.add_argument('-t', type=int,
+                        default=60,
+                        dest="timeout",
+                        help="Maximum execution time, in seconds, per execution loop.")
+    parser.add_argument('-o', type=float,
+                        default=1.0,
+                        dest="target_occ",
+                        help="Desired occupancy of the final grid. Default is 1.0, which just uses all of the allotted time.")
+    parser.add_argument('-p', type=str,
+                        default="out.pdf",
+                        dest="out_pdf",
+                        help="Name of the output pdf file.")
+    parser.add_argument('-a', type=str,
+                        default="basic",
+                        dest="algorithm",
+                        help="The algorithm to use.")
+
+    return parser.parse_args()
+
+
+def create_generator(algorithm, word_list, dimensions, n_loops, timeout, target_occupancy):
+    """ Constructs the generator object for the given algorithm.
+    """
+    algorithm_class_map = {"basic": GridGenerator}
+
+    try:
+        return algorithm_class_map[algorithm](word_list, dimensions, n_loops, timeout,
+                                              target_occupancy)
+    except KeyError:
+        print("Could not create generator object for unknown algorithm: {}.".format(algorithm))
+
+
+def make_crossword(words, size):
+    # Parse args
+    args = parse_cmdline_args()
+
+    # Construct the generator object
+    dim = args.dim if len(args.dim) == 2 else [args.dim[0], args.dim[0]]
+    print(dim)
+    print(type(dim))
+    generator = create_generator(args.algorithm, words, dim, args.n_loops, args.timeout,
+                                 args.target_occ)
+    if not generator:
+        return
+
+    # Generate the grid
+    generator.generate_grid()
+
+    grid = generator.get_grid()
+    words_in_grid = generator.get_words_in_grid()
+    file_ops.write_grid_to_screen(grid, words_in_grid)
+
+
+def get_crosswords():
+    all_words = ['regular', 'violent', 'rebel', 'handy', 'noxious', 'bare', 'rightful', 'chance',
+                 'agonizing', 'mean', 'report', 'harmony', 'barbarous', 'rapid', 'memory',
+                 'vegetable', 'excite', 'illustrious', 'burly', 'old-fashioned', 'field',
+                 'seashore', 'wild', 'skate', 'temporary', 'debonair', 'forgetful', 'film',
+                 'lavish', 'scary']
     max_word_amount = 20
     if len(all_words) > max_word_amount:
         words = rand_words(all_words, max_word_amount)
     else:
         words = all_words.copy()
-    words.sort(key = len, reverse = True)
+    # words.sort(key = len, reverse = True)
+    #
+    # map = [[0] * 15] * 15
+    #
+    # rand_x = random.randint(0, 15 - len(words[0]) - 1)
+    # rand_y = random.randint(0, 15 - len(words[0]) - 1)
+    # rand_direction = randint(0, 1)
+    # placed_words = []
+    # place_word_in_map(word, map, rand_x, rand_y, rand_direction, current_words)
+    #
+    # make_crossword(map, placed_words, words[1:])
+    grid_size = 15
 
-    map = [[0] * 15] * 15
+    print(words)
 
-    rand_x = random.randint(0, 15 - len(words[0]) - 1)
-    rand_y = random.randint(0, 15 - len(words[0]) - 1)
-    rand_direction = randint(0, 1)
-    placed_words = []
-    place_word_in_map(word, map, rand_x, rand_y, rand_direction, current_words)
+    make_crossword(all_words, grid_size)
 
-    make_crossword(map, placed_words, words[1:])
+
+if __name__ == "__main__":
+    print("hello")
+    get_crosswords()
