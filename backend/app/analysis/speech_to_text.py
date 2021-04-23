bd45d091a0c78f7d58e238cc3fc881eda76851ea
sharabhuiyan <sbhuiyan@mit.edu>
2021-04-23 16:16:14 -0400

    Update speech_to_text.py

--- backend/app/analysis/speech_to_text.py
+++ backend/app/analysis/speech_to_text.py
@@ -1,5 +1,14 @@
 import json
+import nltk
 
 
 def parse_json(file):
     pass
+
+
+def sentence_feeder(text):
+    """
+    Given text, returns a list of sentences in the text.
+    """
+    return nltk.tokenize.sent_tokenize(text)
+
