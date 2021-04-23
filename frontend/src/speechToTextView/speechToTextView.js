80a3d2cc809791f53ac5140903a3a184a349128f
Jackie Lin <jackielin655@gmail.com>
2021-04-23 17:00:10 -0400

    added react-mic

--- frontend/src/speechToTextView/speechToTextView.js
+++ frontend/src/speechToTextView/speechToTextView.js
@@ -8,8 +8,26 @@ export class SpeechToTextView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            sentences: [],
+            sentenceIndex: 0,
+            record: false,
         };
+    }
+
+    startRecording = () => {
+        this.setState({ record: true });
+    }
+
+    stopRecording = () => {
+        this.setState({ record: false });
+    }
+
+    onData(recordedBlob) {
+        console.log('chunk of real-time data is: ', recordedBlob);
+    }
+
+    onStop(recordedBlob) {
+        console.log('recordedBlob is: ', recordedBlob);
     }
 
     async componentDidMount() {
@@ -27,7 +45,16 @@ export class SpeechToTextView extends React.Component {
         return (<React.Fragment>
             <Navbar />
             <div className="page">
-                Hello World!
+                <ReactMic
+                    record={this.state.record}
+                    className="sound-wave"
+                    onStop={this.onStop}
+                    onData={this.onData}
+                    strokeColor="#000000"
+                    backgroundColor="#FF4081"
+                />
+                <button onClick={this.startRecording} type="button">Start</button>
+                <button onClick={this.stopRecording} type="button">Stop</button>
             </div>
             <Footer />
         </React.Fragment>);
