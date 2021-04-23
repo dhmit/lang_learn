f1ebac722d2907b5f801d24807a1333d5495d873
Peihua Huang <nemesis2126@gmail.com>
2021-04-23 16:52:31 -0400

    added react-mic

--- frontend/src/speechToTextView/speechToTextView.js
+++ frontend/src/speechToTextView/speechToTextView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
+
+import { ReactMic } from 'react-mic';
 import { Footer, Navbar, LoadingPage } from '../UILibrary/components';
 
 export class SpeechToTextView extends React.Component {
